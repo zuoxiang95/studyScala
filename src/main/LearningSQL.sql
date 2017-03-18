a2d02d97ebc99c99ef2e7e77dd83cd6432cb48e8
Zuoxiang1 <zx_data@jaasun.com>
2017-03-18 12:43:56 +0800

    复习sql语句

--- src/main/LearningSQL.sql
+++ src/main/LearningSQL.sql
@@ -94,29 +94,213 @@ AND "degree" > (
 )
 
 -- 查询score中选学一门以上课程的同学中分数为非最高分成绩的记录。
+SELECT
+	*
+FROM
+	score
+WHERE
+	sno IN (
+		SELECT
+			sno
+		FROM
+			score
+		GROUP BY
+			sno
+		HAVING
+			COUNT (*) > 1
+	)
+AND "degree" NOT IN (
+	SELECT
+		MAX ("degree")
+	FROM
+		score
+	GROUP BY
+		sno
+)
 
+select s2.* from (SELECT sno,MIN(DEGREE)AS m FROM score GROUP BY sno HAVING COUNT(*)>1) as sc,score s2 where sc.sno=s2.sno and sc.m=s2.degree
 -- 查询成绩高于学号为“109”-- 课程号为“3-105”的成绩的所有记录。
+SELECT
+	*
+FROM
+	score
+WHERE
+	"degree" > (
+		SELECT
+			"degree"
+		FROM
+			score
+		WHERE
+			sno = '109'
+		AND cno = '3-105'
+	);
 -- 查询和学号为108的同学同年出生的所有学生的Sno-- Sname和Sbirthday列。
+SELECT
+	sno,
+	sname,
+	sbirthday
+FROM
+	student
+WHERE
+	sbirthday = (
+		SELECT
+			sbirthday
+		FROM
+			student
+		WHERE
+			sno = '108'
+	);
 -- 查询“张旭“教师任课的学生成绩。
+SELECT
+	score.sno,
+	score.cno,
+	score."degree"
+FROM
+	score,
+	course,
+	teacher
+WHERE
+	course.tno = teacher.tno
+AND teacher.tname = '张旭'
+AND course.cno = score.cno;
 -- 查询选修某课程的同学人数多于5人的教师姓名。
+SELECT
+	tname
+FROM
+	teacher,
+	course
+WHERE
+	course.cno = (
+		SELECT
+			cno
+		FROM
+			score
+		GROUP BY
+			cno
+		HAVING
+			COUNT (cno) > 5
+	)
+AND course.tno = teacher.tno
 -- 查询95033班和95031班全体学生的记录。
--- 查询存在有85分以上成绩的课程Cno.
+select * from student where  "class" in ('95033', '95031')
+-- 查询存在有85分以上成绩的课程Cno。
+select distinct cno from score where "degree" > 85
 -- 查询出“计算机系“教师所教课程的成绩表。
+SELECT
+	*
+FROM
+	score
+WHERE
+	cno IN (
+		SELECT
+			cno
+		FROM
+			course,
+			teacher
+		WHERE
+			course.tno = teacher.tno
+		AND teacher.depart = '计算机系'
+	)
 -- 查询“计算机系”与“电子工程系“不同职称的教师的Tname和Prof。
+SELECT
+	tname,
+	prof
+FROM
+	teacher
+WHERE
+	prof NOT IN (
+		SELECT DISTINCT
+			prof
+		FROM
+			teacher
+		WHERE
+			depart = '电子工程系'
+	)
 -- 查询选修编号为“3-105“课程且成绩至少高于选修编号为“3-245”的同学的Cno-- Sno和Degree,并按Degree从高到低次序排序。
--- 查询选修编号为“3-105”且成绩高于选修编号为“3-245”课程的同学的Cno-- Sno和Degree.
--- 查询所有教师和同学的name-- sex和birthday.
--- 查询所有“女”教师和“女”同学的name-- sex和birthday.
+SELECT
+	cno,
+	sno,
+	"degree"
+FROM
+	score
+WHERE
+	"degree" > (
+		SELECT
+			MIN ("degree")
+		FROM
+			score
+		WHERE
+			cno = '3-245'
+	)
+AND cno = '3-105'
+ORDER BY
+	"degree" DESC;
+-- 查询选修编号为“3-105”且成绩高于选修编号为“3-245 ”课程的同学的Cno-- Sno和Degree。
+select  cno, sno, "degree" from score where "degree" > (select max("degree") from score where cno = '3-105') and cno = '3-245'
+-- 查询所有教师和同学的name-- sex和birthday。
+SELECT
+	sname AS NAME,
+	ssex AS sex,
+	sbirthday AS birthday
+FROM
+	student
+UNION
+	SELECT
+		tname,
+		tsex,
+		tbirthday
+	FROM
+		teacher
+-- 查询所有“女”教师和“女”同学的name-- sex和birthday。
+SELECT
+	sname AS NAME,
+	ssex AS sex,
+	sbirthday AS birthday
+FROM
+	student
+where
+    ssex = '女'
+UNION
+	SELECT
+		tname,
+		tsex,
+		tbirthday
+	FROM
+		teacher
+	where
+	    tsex = '女'
 -- 查询成绩比该课程平均成绩低的同学的成绩表。
--- 查询所有任课教师的Tname和Depart.
--- 查询所有未讲课的教师的Tname和Depart.
+SELECT
+	A .*
+FROM
+	score AS A
+WHERE
+	A ."degree" > (
+		SELECT
+			AVG ("degree")
+		FROM
+			score AS b
+		WHERE
+			A .cno = b.cno
+		GROUP BY
+			cno
+	)
+-- 查询所有任课教师的Tname和Depart。
+select tname, depart from teacher, course where teacher.tno = course.tno
+select a.tname, a.depart from teacher as a inner join course as b on a.tno = b.tno
+-- 查询所有未讲课的教师的Tname和Depart。
+select tname, depart from teacher where tno not in (select distinct tno from course);
+select tname, depart from teacher a left join course b using(tno) where b.cno is null;
 -- 查询至少有2名男生的班号。
+select "class" from student where ssex = '男' group by "class" having count(*) > 1;
 -- 查询Student表中不姓“王”的同学记录。
+select * from student where sname not like '王%'
 -- 查询Student表中每个学生的姓名和年龄。
+select sname   from student
 -- 查询Student表中最大和最小的Sbirthday日期值。
 -- 以班号和年龄从大到小的顺序查询Student表中的全部记录。
 -- 查询“男”教师及其所上的课程。
 -- 查询最高分同学的Sno-- Cno和Degree列。
--- 查询和“李军”同性别的所有同学的Sname.
--- 查询和“李军”同性别并同班的同学Sname.
--- 查询所有选修“计算机导论”课程的“男”同学的成绩表
+-- 查询和“李军”同性别的所有同学的Sname。
+-- 查询和“李军”同性别并同班的同学Sname。
+-- 查询所有选修“计算机导论”课程的“男”同学的成绩表。
